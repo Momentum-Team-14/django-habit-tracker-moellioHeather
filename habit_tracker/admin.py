9e04aafde007feea2a03d19cfbf311645059cb8f
heathersmith <moellio.heather@gmail.com>
2022-09-06 19:12:12 -0400

    creates app with config, habit_tracker, and heroku setup

--- habit_tracker/admin.py
+++ habit_tracker/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import CustomUser
 from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', ]
+
+
 admin.site.register(CustomUser, UserAdmin)
