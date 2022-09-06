0df43866b7b7e881b1a02ddfdd93519c1ed6b01f
heathersmith <moellio.heather@gmail.com>
2022-09-06 18:18:14 -0400

    saves changes to settings for heroku setup

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import django_on_heroku
 from pathlib import Path
 import environ
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
